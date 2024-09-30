b2fb3c01c2af33e967a718fea49c796d4597a61b
Emanuele De Rossi <derossiemanuele98@gmail.com>
2024-09-30 16:49:53 +0200

    tailoerd for AI

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -13,26 +13,47 @@ export async function POST(req: Request) {
 
   const result = await streamText({
     model: openai('gpt-4o'),
-    system: 'Your sole porpuse is to help user get meteorological information',
+    system: 'Your sole porpuse is to help user understand wich kind of entity is the user organization in, in the context of the AI ACT framework',
     messages: convertToCoreMessages(messages),
     tools: {
-      getWeather: {
-        description: 'Get the weather for a location',
+      getEntityType: {
+        description: 'Get the type of entity the user organization is in',
         parameters: z.object({
-          city: z.string().describe('The city to get the weather for'),
-          unit: z
-            .enum(['C', 'F'])
-            .describe('The unit to display the temperature in'),
+          entity: z
+            .enum(['Provider', 'Deployer', 'Disruptor', 'Importer', 'Product Manufacturer', 'Authorised Representative'])
+            .describe(`
+            the entity type the user organization is in based on these descriptions: 
+                - Provider: a natural or legal person, public authority, agency or other body that develops an AI system or a general purpose AI model (or that has an AI system or a general purpose AI model developed) and places them on the market or puts the system into service under its own name or trademark, whether for payment or free of charge;
+                - Deployer: any natural or legal person, public authority, agency or other body using an AI system under its authority except where the AI system is used in the course of a personal non-professional activity;
+                - Distributor: any natural or legal person in the supply chain, other than the provider or the importer, that makes an AI system available on the Union market;
+                - Importer: any natural or legal person located or established in the Union that places on the market an AI system that bears the name or trademark of a natural or legal person established outside the Union;
+                - Authorised representative: any natural or legal person located or established in the Union who has received and accepted a written mandate from a provider of an AI system or a general purpose AI model to, respectively, perform and carry out on its behalf the obligations and procedures established by this Regulation.
+                - Product manufacturer: places on the market or puts into service an AI system together with their product and under their own name or trademark;            
+            `),
         }),
-        execute: async ({ city, unit }) => {
-          const weather = {
-            value: 24,
-            description: 'Sunny',
-          };
-
-          return `It is currently ${weather.value}°${unit} and ${weather.description} in ${city}!`;
+        execute: async ({ entity }) => {
+          return `The user organization is a ${entity}`;
         },
       },
+      systemModifications: {
+            description: 'Possible shift of entity type to Provider if the user organization does one of those actions',
+            parameters: z.object({
+            action: z
+                .boolean()
+                .describe(`
+                    the action the user organization does that may shift their entity type to Provider:
+                    - Puts their name on an existing system, 
+                    - Makes significant changes to a system (so that it differs form the original version in a relevant way), 
+                    - Modifies the intended purpose of a system.
+                    Evaluate TRUE if the user organization does one of those actions, otherwise FALSE
+                    `)
+            }),
+            execute: async ({ action }) => {
+                if (action) {
+                    return `The user organization is now cosidered a Provider`;
+                };
+            },
+        },
     },
   });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import { useChat } from 'ai/react';
- 
+
 export default function Chat() {
   const { messages, input, setInput, append, handleSubmit } = useChat({
     api: '/api/chat',
   });
   const toolInvocations = messages.flatMap(m => m.toolInvocations || []);  
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+    <div className="flex flex-col w-full max-w-md py-24 h-[90vh] mx-auto stretch overflow-y-scroll">
       {messages.map(m => (
         <div key={m.id} className="whitespace-pre-wrap">
           {m.role === 'user' ? 'User: ' : 'AI: '}
@@ -16,17 +16,19 @@ export default function Chat() {
         </div>
       ))}
 
-      {toolInvocations.map((invocation, i) => (
-        <div key={invocation.toolCallId} className="whitespace-pre-wrap bg-red-600 p-2 text-white uppercase">
-          {'result' in invocation ? invocation.result : 'No result available'}
-        </div>
-      ))}
- 
-      <form onSubmit={(e)=> {
+      <div className="fixed top-0 right-0 flex flex-col gap-2">
+        {toolInvocations.map((invocation, i) => (
+          <div key={invocation.toolCallId} className="whitespace-pre-wrap bg-red-600 p-2 text-white uppercase">
+            {'result' in invocation ? invocation.result : 'No result available'}
+          </div>
+        ))}
+      </div>
+
+      <form onSubmit={(e) => {
         e.preventDefault()
-        append({content: input, role: "user"})
+        append({ content: input, role: "user" })
         setInput("")
-        }}>
+      }}>
         <input
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
